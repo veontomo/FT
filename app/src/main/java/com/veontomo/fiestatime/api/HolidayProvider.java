e8a2623ff4956e96a644c63e99dcd0fc3a82d3c8
Mario Rossi <veontomo@gmail.com>
2015-10-26 07:28:39 +0100

    dumb commit

--- app/src/main/java/com/veontomo/fiestatime/api/HolidayProvider.java
+++ app/src/main/java/com/veontomo/fiestatime/api/HolidayProvider.java
@@ -22,29 +22,6 @@ public class HolidayProvider {
 
 
     public Holiday next(Context context){
-        com.google.api.services.calendar.Calendar client = null;
-        ArrayList<String> l = new ArrayList<>();
-        l.add(CalendarScopes.CALENDAR);
-        credential = GoogleAccountCredential.usingOAuth2(context, l);
-//        credential.setSelectedAccountName(null);
-        client = getCalendarService(credential);
-        String pageToken = null;
-        com.google.api.services.calendar.model.Events events;
-        do {
-            try {
-                events = client.events().list("en.usa#holiday@group.v.calendar.google.com").execute();
-//                events = client.events().list("en.usa#holiday@group.v.calendar.google.com").setPageToken(pageToken).execute();
-                pageToken = events.getNextPageToken();
-                java.util.List<com.google.api.services.calendar.model.Event> list = events.getItems();
-                Log.i(Config.APP_NAME, "event list contains " + list.size() + " elements");
-                for (com.google.api.services.calendar.model.Event event : list) {
-                    Log.i(Config.APP_NAME, "description: " + event.getDescription());
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } while (pageToken != null);
         return new Holiday("A holiday", System.currentTimeMillis() + 1000*60*10);
     }
 
