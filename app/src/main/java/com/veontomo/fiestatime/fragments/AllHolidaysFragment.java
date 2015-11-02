69ae26a7f84d68f3ae4a4e25d41c66910044aba5
Mario Rossi <veontomo@gmail.com>
2015-11-02 08:27:35 +0100

    feat: make the fragments not overlap

--- app/src/main/java/com/veontomo/fiestatime/fragments/AllHolidaysFragment.java
+++ app/src/main/java/com/veontomo/fiestatime/fragments/AllHolidaysFragment.java
@@ -30,10 +30,7 @@ public class AllHolidaysFragment extends ListFragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-//        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-//                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-//                "Linux", "OS/2" };
-        String[] values = new String[] {};
+        String[] values = new String[] { "New Year", "14 February", "8 march"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
                 android.R.layout.simple_list_item_1, values);
         setListAdapter(adapter);
