840d32d007f9e8c40ba49a198087da0aa429d3e8
Mario Rossi <veontomo@gmail.com>
2015-11-02 08:12:25 +0100

    feat: set up "empty" view for the list

--- app/src/main/java/com/veontomo/fiestatime/fragments/AllHolidaysFragment.java
+++ app/src/main/java/com/veontomo/fiestatime/fragments/AllHolidaysFragment.java
@@ -1,12 +1,13 @@
 package com.veontomo.fiestatime.fragments;
 
-import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.app.ListFragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.veontomo.fiestatime.Logger;
 import com.veontomo.fiestatime.R;
@@ -14,17 +15,28 @@ import com.veontomo.fiestatime.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AllHolidaysFragment.OnFragmentInteractionListener} interface
+ * {@link AllHolidaysFragment.AllHolidaysActions} interface
  * to handle interaction events.
  */
-public class AllHolidaysFragment extends Fragment {
+public class AllHolidaysFragment extends ListFragment {
 
 
-
-    private OnFragmentInteractionListener mHostActivity;
+    private AllHolidaysActions mHostActivity;
 
     public AllHolidaysFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+//        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+//                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+//                "Linux", "OS/2" };
+        String[] values = new String[] {};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
     }
 
 
@@ -35,27 +47,20 @@ public class AllHolidaysFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_all_holidays, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mHostActivity != null) {
-            mHostActivity.onFragmentInteraction(uri);
-        }
-    }
-
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-    }
-
-    @Override
-    public void onStart(){
+    public void onStart() {
+        super.onStart();
         try {
-            mHostActivity = (OnFragmentInteractionListener) getActivity();
+            mHostActivity = (AllHolidaysActions) getActivity();
         } catch (ClassCastException e) {
             Logger.log("AllHolidaysFragment is embedded to an activity that does not support interaction");
             mHostActivity = null;
         }
+    }
 
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO implement some logic
     }
 
     @Override
@@ -74,9 +79,8 @@ public class AllHolidaysFragment extends Fragment {
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
      */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+    public interface AllHolidaysActions {
+
     }
 
 }
