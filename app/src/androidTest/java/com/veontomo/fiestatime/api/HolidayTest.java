9e6d43be7d69f02ce46b6892cb08376368f09649
Mario Rossi <veontomo@gmail.com>
2015-12-02 15:16:07 +0100

    test: modify tests of the Holiday class

--- app/src/androidTest/java/com/veontomo/fiestatime/api/HolidayTest.java
+++ app/src/androidTest/java/com/veontomo/fiestatime/api/HolidayTest.java
@@ -7,92 +7,40 @@ import junit.framework.TestCase;
  */
 public class HolidayTest extends TestCase {
 
-    Holiday h1, h2;
+    SingleHoliday h1, h2;
 
     public void setUp() throws Exception {
         super.setUp();
-        h1 = new Holiday(22, "Saturday night", 12345, 3);
-        h2 = new Holiday("Xmas", 834, 1);
+        h1 = new SingleHoliday(22, "Saturday night", 12345);
+        h2 = new SingleHoliday("Xmas", 834);
     }
 
-    public void testHolidayExistent() {
+    public void testSingleHolidayExistent() {
         assertEquals("id must be equal to 22", h1.id, 22);
     }
 
-    public void testHolidayWithoutId() {
+    public void testSingleHolidayWithoutId() {
         assertEquals("id must be equal to -1", h2.id, -1);
     }
 
-    public void testHolidayNameExistent() {
-        assertEquals("holiday name must be equal to Saturday night", h1.name, "Saturday night");
+    public void testSingleHolidayNameExistent() {
+        assertEquals("SingleHoliday name must be equal to Saturday night", h1.name, "Saturday night");
     }
 
-    public void testHolidayNameWithoutId() {
-        assertEquals("holiday name must be equal to Saturday night", h2.name, "Xmas");
+    public void testSingleHolidayNameWithoutId() {
+        assertEquals("SingleHoliday name must be equal to Saturday night", h2.name, "Xmas");
     }
 
-    public void testHolidayNextExistent() {
-        assertEquals("holiday next occurrence must be equal to 12345", h1.nextOccurrence, 12345);
+    public void testSingleHolidayNextExistent() {
+        assertEquals("SingleHoliday next occurrence must be equal to 12345", h1.nextOccurrence, 12345);
     }
 
-    public void testHolidayNextWithoutId() {
-        assertEquals("holiday next occurrence must be equal to 12345", h2.nextOccurrence, 834);
-    }
-
-    public void testHolidayPeriodicityExistent() {
-        assertEquals("holiday periodicity must be equal to 3", h1.periodicity, 3);
-    }
-
-    public void testHolidayPeriodicityWithoutId() {
-        assertEquals("holiday periodicity must be equal to 1", h2.periodicity, 1);
-    }
-
-    public void testDeserializeIdExistent() {
-        Holiday h = Holiday.deserialize(h1.serialize());
-        assertEquals("after deserialization, holiday id is wrong!", h.id, 22);
-    }
-
-    public void testDeserializeNameExistent() {
-        Holiday h = Holiday.deserialize(h1.serialize());
-        assertEquals("after deserialization, holiday name is wrong!", h.name, "Saturday night");
-    }
-
-    public void testDeserializeOccurrenceExistent() {
-        Holiday h = Holiday.deserialize(h1.serialize());
-        assertEquals("after deserialization, holiday next occurrence is wrong!", h.nextOccurrence, 12345);
-    }
-
-    public void testDeserializePeriodicityExistent() {
-        Holiday h = Holiday.deserialize(h1.serialize());
-        assertEquals("after deserialization, holiday periodicity is wrong!!", h.periodicity, 3);
-    }
-
-    public void testDeserializeIdOfNonExistent() {
-        Holiday h = Holiday.deserialize(h2.serialize());
-        assertEquals("after deserialization, holiday id is wrong!", h.id, -1);
-    }
-
-    public void testDeserializeNameOfNonExistent() {
-        Holiday h = Holiday.deserialize(h2.serialize());
-        assertEquals("after deserialization, holiday name is wrong!", h.name, "Xmas");
-    }
-
-    public void testDeserializeOccurrenceOfNonExistent() {
-        Holiday h = Holiday.deserialize(h2.serialize());
-        assertEquals("after deserialization, holiday next occurrence is wrong!", h.nextOccurrence, 834);
-    }
-
-    public void testDeserializePeriodicityOfNonExistent() {
-        Holiday h = Holiday.deserialize(h2.serialize());
-        assertEquals("after deserialization, holiday periodicity is wrong!", h.periodicity, 1);
+    public void testSingleHolidayNextWithoutId() {
+        assertEquals("SingleHoliday next occurrence must be equal to 12345", h2.nextOccurrence, 834);
     }
 
 
-    public void testDeserializeExistentHolidayWithoutName() {
-        Holiday noName = new Holiday(4, null, 355, 0);
-        Holiday h = Holiday.deserialize(noName.serialize());
-        assertEquals("after deserialization, holiday name is wrong!", "", h.name);
-    }
+
 
 
 
