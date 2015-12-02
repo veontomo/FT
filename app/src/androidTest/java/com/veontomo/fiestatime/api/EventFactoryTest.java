6cdf8a80dcb7aab4304cf1012b99485da969bf96
Mario Rossi <veontomo@gmail.com>
2015-12-02 16:56:14 +0100

    feat: stub event factory methods

---
+++ app/src/androidTest/java/com/veontomo/fiestatime/api/EventFactoryTest.java
@@ -0,0 +1,21 @@
+package com.veontomo.fiestatime.api;
+
+import junit.framework.TestCase;
+
+/**
+ * Test for the event factory
+ */
+public class EventFactoryTest extends TestCase {
+
+    public void testProduce() throws Exception {
+
+    }
+
+    public void testProduce1() throws Exception {
+
+    }
+
+    public void testIndexOf() throws Exception {
+
+    }
+}
