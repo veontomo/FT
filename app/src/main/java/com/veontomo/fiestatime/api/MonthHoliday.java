cc8034a2b0c395a49b24e08ce2006a690ab93e5f
Mario Rossi <veontomo@gmail.com>
2015-12-02 08:08:43 +0100

    feat: create month event

---
+++ app/src/main/java/com/veontomo/fiestatime/api/MonthHoliday.java
@@ -0,0 +1,34 @@
+package com.veontomo.fiestatime.api;
+
+import java.util.Calendar;
+
+/**
+ * Represents a holiday with monthly periodicity
+ *
+ */
+public class MonthHoliday extends Holiday {
+    /**
+     * Set the holiday date such that its next occurrence is after given time based in its periodicity.
+     *
+     * @param time time in milliseconds
+     */
+    @Override
+    public void adjustDate(long time) {
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(this.nextOccurrence);
+        while (nextOccurrence < time) {
+            c.add(Calendar.MONTH, 1);
+            nextOccurrence = c.getTimeInMillis();
+        }
+    }
+
+    /**
+     * Returns true if the holiday date should be adjusted and false otherwise.
+     *
+     * @param time
+     */
+    @Override
+    public boolean shouldAdjustDate(long time) {
+        return nextOccurrence < time;
+    }
+}
