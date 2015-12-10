19a1bdeb90a85aff339a82eb7714c90117af10d9
Mario Rossi <veontomo@gmail.com>
2015-12-10 15:50:01 +0100

    test: use renaming delagating context for storage testing

--- app/src/androidTest/java/com/veontomo/fiestatime/api/StorageTest.java
+++ app/src/androidTest/java/com/veontomo/fiestatime/api/StorageTest.java
@@ -1,6 +1,9 @@
 package com.veontomo.fiestatime.api;
 
 import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
+
+import static org.assertj.core.api.Assertions.*;
 
 
 /**
@@ -13,18 +16,41 @@ public class StorageTest extends AndroidTestCase {
 
     public void setUp() throws Exception {
         super.setUp();
-        storage = new Storage(getContext());
+        RenamingDelegatingContext context = new RenamingDelegatingContext(getContext(), "test_");
+        storage = new Storage(context);
     }
 
     public void tearDown() throws Exception {
-
+        storage.close();
+        storage = null;
+        super.tearDown();
     }
 
     public void testSaveSingleEvent(){
         SingleEvent e = new SingleEvent("single event", 1234567000L);
         long id = storage.save(e);
-        assertTrue(id != -1);
+        assertThat(id).isNotEqualTo(-1);
     }
 
+
+    public void testSaveTwoEventsWithTheSameName(){
+        String name = "plain event";
+        SingleEvent e1 = new SingleEvent(name, 9934567000L);
+        SingleEvent e2 = new SingleEvent(name, 4454567000L);
+        long id1 = storage.save(e1);
+        long id2 = storage.save(e2);
+        assertThat(id1).isNotEqualTo(-1);
+        assertThat(id2).isNotEqualTo(-1);
+    }
+
+    public void testSaveTheSameEventTwice(){
+        Event e = new SingleEvent(14, "an event", 3489547858L);
+        long id1 = storage.save(e);
+        long id2 = storage.save(e);
+        assertThat(id1).isNotEqualTo(-1);
+        assertThat(id2).isEqualTo(-1);
+
+
+    }
 
 }
