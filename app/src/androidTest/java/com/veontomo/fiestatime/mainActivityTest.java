fda607378b4c492d0a67c7c99a2c54bcce093974
Mario Rossi <veontomo@gmail.com>
2015-12-14 22:29:51 +0100

    test: start Robolectric tests

---
+++ app/src/androidTest/java/com/veontomo/fiestatime/mainActivityTest.java
@@ -0,0 +1,57 @@
+package com.veontomo.fiestatime;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+
+/**
+ * Created by Mario Rossi on 14/12/2015 at 22:27.
+ *
+ * @author veontomo@gmail.com
+ * @since 0
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = 21)
+public class mainActivityTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testOnCreate() throws Exception {
+
+    }
+
+    public void testOnStart() throws Exception {
+
+    }
+
+    public void testOnCreateOptionsMenu() throws Exception {
+
+    }
+
+    public void testOnOptionsItemSelected() throws Exception {
+
+    }
+
+    public void testOnHolidayAdded() throws Exception {
+
+    }
+
+    public void testOnHolidayUpdated() throws Exception {
+
+    }
+
+    public void testOnHolidayClicked() throws Exception {
+
+    }
+}
