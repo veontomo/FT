6689d5cc39a4c67d63f4f71b0ca3eaf3017c501c
Mario Rossi <veontomo@gmail.com>
2016-02-22 16:20:20 +0100

    feat: create the task background process

--- app/src/main/java/com/veontomo/fiestatime/api/RetrieveFromCalendarTask.java
+++ app/src/main/java/com/veontomo/fiestatime/api/RetrieveFromCalendarTask.java
@@ -12,7 +12,7 @@ import com.veontomo.fiestatime.mainActivity;
 /**
  * Retrieves the events from the calendar and stores them into the storage
  */
-public class RetrieveFromCalendarTask extends AsyncTask<Void, Integer, Void> {
+public class RetrieveFromCalendarTask extends AsyncTask<Void, Float, Void> {
 
     private final Context mContext;
     private final mainActivity mCaller;
@@ -24,30 +24,34 @@ public class RetrieveFromCalendarTask extends AsyncTask<Void, Integer, Void> {
 
     @Override
     protected Void doInBackground(Void... params) {
+        Logger.log("RetrieveFromCalendarTask background");
         ContentResolver contentResolver = this.mContext.getContentResolver();
 //        Cursor cursor = contentResolver.query(Uri.parse("content://com.android.calendar/events"),
 //                (new String[]{"calendar_id", "title", "description", "dtstart", "dtend", "eventTimezone", "eventLocation"}), "(" + "dtstart" + "> 0)", null, "dtstart ASC");
         Cursor cursor = contentResolver.query(Uri.parse("content://com.android.calendar/events"),
                 null, "(" + "dtstart" + "> 0)", null, "dtstart ASC");
+        int rows = cursor.getCount();
+        int currentRowIndex = 1;
         if (cursor != null) {
             Logger.log("cursor size = " + cursor.getCount());
             int col = cursor.getColumnCount();
             StringBuilder builder;
             while (cursor.moveToNext()) {
                 builder = new StringBuilder();
-                publishProgress(cursor.getCount());
+
                 for (int i = 0; i < col; i++) {
                     builder.append(cursor.getColumnName(i));
                     builder.append(": ");
                     builder.append(cursor.getString(i));
                     builder.append(" ");
-
                 }
                 builder.append(System.getProperty("line.separator", " "));
                 Logger.log("title: " + cursor.getString(cursor.getColumnIndex("title")));
                 Logger.log("description: " + cursor.getString(cursor.getColumnIndex("description")));
                 Logger.log("dtstart: " + cursor.getString(cursor.getColumnIndex("dtstart")));
                 Logger.log(builder.toString());
+                publishProgress(((float) currentRowIndex)/rows);
+                currentRowIndex++;
             }
             cursor.close();
 
@@ -55,11 +59,12 @@ public class RetrieveFromCalendarTask extends AsyncTask<Void, Integer, Void> {
         return null;
     }
 
-    protected void onProgressUpdate(Integer... progress) {
+    protected void onProgressUpdate(Float... progress) {
         Logger.log("progress updates" + String.valueOf(progress[0]));
     }
 
-    protected void onPostExecute(){
+    @Override
+    public void onPostExecute(Void v){
         Logger.log("loaded");
         this.mCaller.onEventsImported();
     }
